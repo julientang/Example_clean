c06bbd7deb014780d62400f49588f1417dca90f6
Julien TANG <julien.tang@client-172-18-107-34.eduroam.universite-paris-saclay.fr>
2025-06-03 12:34:10 +0200

    Added Unit tests

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'mycosmo'
+copyright = '2025, Julien TANG'
+author = 'Julien TANG'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    "python": ("http://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
--- mycosmo/cosmology.py
+++ mycosmo/cosmology.py
@@ -1,7 +1,55 @@
 import numpy as np
-from .constants import Mpc, G
+
+from .constants import G, Mpc
+
+"""Cosmology.
+
+This module implements various cosmology routines.
+
+"""
+
 
 def hubble(redshift, cosmo_dict):
+    """Hubble Parameter.
+
+    Calculate the Hubble parameter at a given redshift using the cosmological parameter values provided.
+
+    Parameters
+    ----------
+    redshift : float or numpy.ndarray
+        Redshift(s) at which the Hubble parameter should be calculated
+    cosmo_dict : dict
+        Dictionary of cosmological constants. 
+
+    cosmo_dict : dict
+        Dictionary of cosmological constants. Must contain the following keys:
+
+        * ``H0``: The Hubble parameter value at redshift zero.
+        * ``omega_m_0``: The matter density at redshift zero.
+        * ``omega_k_0``: The curvature density at redshift zero.
+        * ``omega_lambda_0``: The dark energy density at redshift zero.
+    
+    Returns
+    -------
+    float or numpy.ndarray
+        Value of the Hubble parameter (km/s/Mpc) at the specified redshift(s) for a given cosmology.
+    
+    Notes
+    -----
+    This function implements the calculation of the Hubble parameter as follows:
+
+    .. math::
+        H(z) = \sqrt{H_0^2 (\Omega_{m,0}(1+z)^3 + \Omega_{k,0}(1+z)^2 +
+            \Omega_{\Lambda,0})}
+    
+    Example
+    -------
+    >>> from mycosmo.cosmology import hubble
+    >>> cosmo_dict = {"H0": 70, "omega_m_0": 0.3, "omega_k_0": 0.0, "omega_lambda_0": 0.7}
+    >>> hubble(0.0, cosmo_dict)
+    np.float64(70.0)
+    """
+
     hubble_const = cosmo_dict["H0"]
     matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
     curvature = cosmo_dict["omega_k_0"] * (1 + redshift) ** 2
@@ -9,8 +57,8 @@ def hubble(redshift, cosmo_dict):
 
     return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
 
+
 def critical_density(redshift, cosmo_dict):
     H_z_si = hubble(redshift, cosmo_dict) * 1e3 / Mpc
 
     return (3.0 * H_z_si**2) / (8.0 * np.pi * G)
-
